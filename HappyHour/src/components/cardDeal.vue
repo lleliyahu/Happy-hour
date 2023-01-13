4100cec3807c8652c00e894c171df835254ca9a4
lleliyahu <lleliyahu@gmail.com>
2023-01-13 11:03:59 +0200

    change card

--- HappyHour/src/components/cardDeal.vue
+++ HappyHour/src/components/cardDeal.vue
@@ -1,17 +1,46 @@
 <template>
   <div>
-    <q-card class="my-card my-content">
-    <div class="my-content">
-        <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
-          <div class="absolute-bottom text-h6">
-            {{ store }}
-          </div>
-        </q-img>
+    <q-card class="my-card">
+        <q-img src="https://cdn.quasar.dev/img/chicken-salad.jpg" />
+
         <q-card-section>
-        {{ message }}
+          <q-btn
+            fab
+            color="primary"
+            icon="place"
+            class="absolute"
+            style="top: 0; right: 12px; transform: translateY(-50%);"
+          />
+
+          <div class="row no-wrap items-center">
+            <div class="col text-h6 ellipsis">
+              {{ store }}
+            </div>
+            <div class="col-auto text-grey text-caption q-pt-md row no-wrap items-center">
+              <q-icon name="place" />
+              250 ft
+            </div>
+          </div>
+
+          <q-rating v-model="stars" :max="5" size="32px" />
         </q-card-section>
-      </div>
-    </q-card>
+
+        <q-card-section class="q-pt-none">
+          <div class="text-subtitle1">
+            $10 {{message}}
+          </div>
+          <div class="text-caption text-grey">
+            Small plates, salads & sandwiches in an intimate setting.
+          </div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right">
+          <q-btn v-close-popup flat color="primary" label="Reserve" />
+          <q-btn v-close-popup flat color="primary" round icon="event" />
+        </q-card-actions>
+      </q-card>
   </div>
 </template>
 <script>
